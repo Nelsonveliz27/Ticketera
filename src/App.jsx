76c2dad52fa91ba0263a95d6e6a52be71c2cccc5
Nelsonveliz27 <velizn736@gmail.com>
2023-11-23 09:17:46 -0300

    primer commit ticketera

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar';
+import './App.css'
+
+function App() {
+  return (
+    
+      <div>
+           <NavBar/>;
+      </div>
+     
+  )
+}
+
+export default App
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return (
+    <div>
+        <h1>Eventos</h1>
+        <input placeholder="Busca tu evento favorito" />
+    </div>
+    )
+};
+
+export default NavBar;
