ffa58e1f64a0d9761e86e49b741b8af253e36008
Nelsonveliz27 <velizn736@gmail.com>
2023-11-24 02:05:30 -0300

    segundo commit avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
+import Events from './components/Events';
 import './App.css'
 
 function App() {
   return (
     
       <div>
-           <NavBar/>;
+           <NavBar/>
+           <Events/>
       </div>
      
   )
